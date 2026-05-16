77f8c615658d9fcd658fb1e48b50e4ab1cce7452
dan.lilian <Ldan1307@gmail.com>
2026-05-17 01:32:29 +0200

    feat: add click_link and read_banner_text to NotificationPage

--- src/framework/pages/notification_page.py
+++ src/framework/pages/notification_page.py
@@ -6,4 +6,13 @@ class NotificationPage(BasePage):
     PATH: str = "/notification_message_rendered"
 
     def wait_for_loaded(self) -> None:
-        expect(self.page.get_by_role("heading", name="Notification Message")).to_be_visible()
+        """Wait until the 'Click here" link test is loaded and visible"""
+        expect(self.page.get_by_role("link", name="Click here")).to_be_visible()
+
+    def click_link(self) -> None:
+        """Click the 'Click here' link that triggers the notification banner."""
+        self.page.get_by_role("link", name="Click here").click()
+
+    def read_banner_text(self) -> str:
+        """Return the current notification banner text."""
+        return self.page.locator("#flash").inner_text()
