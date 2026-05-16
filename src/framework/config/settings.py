ee41707573b762e1f88bee40f8efb94130c46297
dan.lilian <Ldan1307@gmail.com>
2026-05-16 19:22:27 +0200

    add BasePage with basic methods

--- src/framework/config/settings.py
+++ src/framework/config/settings.py
@@ -1,3 +1,4 @@
 import os
 
-BASE_URL: str = os.getenv("BASE_URL", "https://the-internet.herokuapp.com")
+# TODO: add comment
+BASE_URL: str = os.getenv("BASE_URL", "https://the-internet.herokuapp.com/")
